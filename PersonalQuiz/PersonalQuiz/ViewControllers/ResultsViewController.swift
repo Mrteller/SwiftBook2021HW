35465ce416ab37f2cda19419504e236c2dbb6930
Pavel <pav.tolmach@gmail.com>
2021-12-21 17:55:24 +0300

    Added a degree of result probability based on relative question score.

--- PersonalQuiz/PersonalQuiz/ViewControllers/ResultsViewController.swift
+++ PersonalQuiz/PersonalQuiz/ViewControllers/ResultsViewController.swift
@@ -30,6 +30,9 @@ class ResultsViewController: UIViewController {
             updateResults()
         }
     }
+    // Should be `allAnsweredQuestions` instead provided by previous screen.
+    // Because some questions could have been skipped.
+    var allQuestions = Question.getQuestions()
     
     // MARK: - Lifecycle methods
     
@@ -44,19 +47,32 @@ class ResultsViewController: UIViewController {
     private func updateResults() {
         let resultsScore = Dictionary(answers.map{ ($0.type, 1) }) { $0 + $1 }
         
-        #if DEBUG
+#if DEBUG
         answers.forEach{ print("\"\($0.title)\" counts for \($0.type.rawValue)") }
         resultsScore.forEach{ print("\($0.key) score is \($0.value)")}
-        #endif
+        
+        // Alernative way to calculate the highest score answer
+        let cs = NSCountedSet(array: answers.map(\.type))
+        if let altResultWithHigherstScore = cs.allObjects.map({ ($0 as! AnimalType, cs.count(for: $0)) }).sorted(by: { $0.1 > $1.1 }).first {
+            print("altResultWithHigherstScore (\(altResultWithHigherstScore.0.rawValue), \(altResultWithHigherstScore.1))")
+        }
+#endif
         
         if let resultWithHigherstScore = resultsScore.map({ ($0.key, $0.value) })
             .sorted(by: { $0.1 > $1.1 })
             .first {
+            guard let possiblePoints = totalPossiblePoints(for: resultWithHigherstScore.0) else { return }
+            let percents = lrint((Double(resultWithHigherstScore.1) / Double(possiblePoints)) * 100)
             // Todo: use percents instead of points
-            resultLabel.text = "Вы \(resultWithHigherstScore.0.rawValue)\n на " + String(format: NSLocalizedString("%d point(s)", comment: ""), resultWithHigherstScore.1)
+            resultLabel.text = "Вы \(resultWithHigherstScore.0.rawValue)\n на \(String(format: NSLocalizedString("%d percent(s)", comment: ""), percents))"
             resultDescriptionLabel.text = resultWithHigherstScore.0.definition
         }
-        
+    }
+    
+    private func totalPossiblePoints(for type: AnimalType) -> Int? {
+        let allAnswers = allQuestions.flatMap{ $0.answers }
+        let answerCount = Dictionary(allAnswers.map{ ($0.type, 1) }) { $0 + $1 }
+        return answerCount[type]
     }
         
 }
