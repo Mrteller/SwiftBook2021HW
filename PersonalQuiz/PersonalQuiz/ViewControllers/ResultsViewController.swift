ef11adb88f566705b4135f300074776e19cdf0f3
Pavel <pav.tolmach@gmail.com>
2021-12-21 12:48:52 +0300

    Solved

---
+++ PersonalQuiz/PersonalQuiz/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ResultsViewController.swift
+//  PersonalQuiz
+//
+//  Created by brubru on 20.12.2021.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. Отобразить результат в соответсвии с этим животным
+    // 4. Избавиться от кнопки возврата назад на экран результатов
+    
+    // MARK: - @IBOutlets
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var resultDescriptionLabel: UILabel!
+    
+    // MARK: - Public vars
+    
+    // I would rather pass just an array of types to make the controller more universal
+    // like this `var answers = [String]() {`
+    var answers = [Answer]() {
+        didSet {
+            guard resultLabel != nil && resultDescriptionLabel != nil else { return }
+            updateResults()
+        }
+    }
+    
+    // MARK: - Lifecycle methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        updateResults()
+    }
+    
+    // MARK: - Private funcs
+    
+    private func updateResults() {
+        let resultsScore = Dictionary(answers.map{ ($0.type, 1) }) { $0 + $1 }
+        
+        #if DEBUG
+        answers.forEach{ print("\($0.title) counts for \($0.type.rawValue)") }
+        resultsScore.forEach{ print("\($0.key) score is \($0.value)")}
+        #endif
+        
+        if let resultWithHigherstScore = resultsScore.map({ ($0.key, $0.value) })
+            .sorted(by: { $0.1 > $1.1 ? true : $0.0.rawValue > $1.0.rawValue })
+            .first {
+            resultLabel.text = "Вы \(resultWithHigherstScore.0.rawValue) на \(resultWithHigherstScore.1)"
+        }
+    }
+        
+}
