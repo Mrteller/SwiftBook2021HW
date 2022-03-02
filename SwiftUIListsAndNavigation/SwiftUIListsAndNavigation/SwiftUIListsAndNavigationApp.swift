3d06d4aed2e176c3d359eae884071a6d43e6d22e
Pavel <pav.tolmach@gmail.com>
2022-03-02 19:06:09 +0300

    SwiftUIListsAndNavigation initial commit.

---
+++ SwiftUIListsAndNavigation/SwiftUIListsAndNavigation/SwiftUIListsAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListsAndNavigationApp.swift
+//  SwiftUIListsAndNavigation
+//
+//  Created by  Paul on 02.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListsAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
